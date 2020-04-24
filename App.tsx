119f9cf1198a69c3a2aa2bdb7249220de84d8fbf
giuliobosco <giuliobva@gmail.com>
2020-04-24 23:14:25 +0200

    fix App.tsx - load MainComponent

--- App.tsx
+++ App.tsx
@@ -1,41 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-import NetInfo from '@react-native-community/netinfo';
+import MainComponent from './app/components/main.component/MainComponent'
 
 export default class App extends Component {
-  state = {
-    isConnected: null,
-  };
-
-  componentDidMount() {
-    NetInfo.fetch().then(state => {
-      this.setState({isConnected: state.isConnected});
-    });
-  }
-
   render() {
-    if (this.state.isConnected) {
-      return (
-        <View style={styles.container}>
-          <Text>Connected</Text>
-        </View>
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          <Text>Not Connected</Text>
-        </View>
-      );
-    }
+    return(
+      <MainComponent/>
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
