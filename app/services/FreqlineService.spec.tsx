7707f3e101028ae0a1bdd4b3901d9ac039888f40
giuliobosco <giuliobva@gmail.com>
2020-05-04 19:40:21 +0200

    add freqlineService test

---
+++ app/services/FreqlineService.spec.tsx
@@ -0,0 +1,97 @@
+import fetchMock from 'fetch-mock';
+
+import Config from '../config'
+import freqlineService from './FreqlineService';
+
+describe('Service::FreqlineService', () => {
+    // testing getApiBase Get freqlineStatus
+    it('should call api and return JSON', () => {
+        const response = {
+            freqline: true,
+            serial: true,
+            db: true
+        }
+
+        fetchMock.mock({
+            name: 'action/freqline/ok',
+            matcher: Config.getApiBase() + 'action/freqline',
+            method: 'GET',
+            response: {
+                status: 200,
+                body: response
+            }
+        })
+
+        freqlineService().then(data => {
+            expect(data).toEqual(response)
+            fetchMock.reset();
+        })
+    })
+
+    it('should call thenFunction and should\'t call catchFunction', async () => {
+        fetchMock.mock({
+            name: 'action/freqline/ok_methods',
+            matcher: Config.getApiBase() + 'action/freqline',
+            method: 'GET',
+            response: {
+                status: 200,
+                body: {
+                    freqline: true,
+                    serial: true,
+                    db: true
+                }
+            }
+        })
+
+        const thenFunction = jest.fn();
+        const catchFunction = jest.fn();
+
+        await freqlineService().then(thenFunction).catch(catchFunction)
+
+        expect(thenFunction).toHaveBeenCalled();
+        expect(catchFunction).not.toHaveBeenCalled();
+
+        fetchMock.reset()
+    })
+
+    it('should call api and retrive an error', () => {
+        const status:number = 400;
+
+        fetchMock.mock({
+            name: 'action/freqline/no',
+            matcher: Config.getApiBase() + 'action/freqline',
+            method: 'GET',
+            response: {
+                status: status,
+                body: []
+            }
+        })
+
+        freqlineService().catch(error => {
+            expect(error.status).toEqual(status);
+            fetchMock.reset();
+        })
+    })
+
+    it('should\'t call thenFunction and should call catchFunction', async () => {
+        fetchMock.mock({
+            name: 'action/freqline/not_methods',
+            matcher: Config.getApiBase() + 'action/freqline',
+            method: 'GET',
+            response: {
+                status: 400,
+                body: []
+            }
+        })
+
+        const thenFunction = jest.fn();
+        const catchFunction = jest.fn();
+
+        await freqlineService().then(thenFunction).catch(catchFunction)
+
+        expect(thenFunction).not.toHaveBeenCalled();
+        expect(catchFunction).toHaveBeenCalled();
+
+        fetchMock.reset();
+    })
+})
