2d1164bfa7893b3b743ffd8ea70b37ebce542936
giuliobosco <giuliobva@gmail.com>
2020-05-04 14:55:24 +0200

    clenup netInfoSlice.spec.tsx

--- app/components/main.component/netInfoSlice.spec.tsx
+++ app/components/main.component/netInfoSlice.spec.tsx
@@ -3,38 +3,38 @@ import netInfo, { netInfoStartCheck, netInfoSuccessCheck } from './netInfoSlice'
 describe('netInfo reducer', () => {
     // testing initial reducer state
     it('should handle initial state', () => {
-        expect(netInfo(undefined, {})).toEqual({isLoading:false,isConnected:false})
+        expect(netInfo(undefined, {})).toEqual({ isLoading: false, isConnected: false })
     })
 
     // testing netInfoStartCheck action 
     it('should handle netInfoStartCheck', () => {
         expect(
-            netInfo(undefined,{
+            netInfo(undefined, {
                 type: netInfoStartCheck
             })
-        ).toEqual({isLoading:true, isConnected:false});
+        ).toEqual({ isLoading: true, isConnected: false });
     })
 
     // testing netInfoSuccessCheck action
     it('should handle netInfoSuccessCheck', () => {
         // testing netInfoSuccessCheck action with network connected
         expect(
-            netInfo(undefined,{
+            netInfo(undefined, {
                 type: netInfoSuccessCheck,
                 payload: {
                     isConnected: false,
                 }
             })
-        ).toEqual({isLoading:false,isConnected:false})
+        ).toEqual({ isLoading: false, isConnected: false })
 
         // testing netInfoSuccessCheck action with out network connected
         expect(
-            netInfo(undefined,{
+            netInfo(undefined, {
                 type: netInfoSuccessCheck,
                 payload: {
                     isConnected: true,
                 }
             })
-        ).toEqual({isLoading:false,isConnected:true})
+        ).toEqual({ isLoading: false, isConnected: true })
     })
 })
