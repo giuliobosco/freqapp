98ec24d8998d928b0f9e9bdb0c80d5d293c0b3e6
giuliobosco <giuliobva@gmail.com>
2020-05-04 14:31:41 +0200

    add netInfoSlice.spec.tsx

---
+++ app/components/main.component/netInfoSlice.spec.tsx
@@ -0,0 +1,40 @@
+import netInfo, { netInfoStartCheck, netInfoSuccessCheck } from './netInfoSlice';
+
+describe('netInfo reducer', () => {
+    // testing initial reducer state
+    it('should handle initial state', () => {
+        expect(netInfo(undefined, {})).toEqual({isLoading:false,isConnected:false})
+    })
+
+    // testing netInfoStartCheck action 
+    it('should handle netInfoStartCheck', () => {
+        expect(
+            netInfo(undefined,{
+                type: netInfoStartCheck
+            })
+        ).toEqual({isLoading:true, isConnected:false});
+    })
+
+    // testing netInfoSuccessCheck action
+    it('should handle netInfoSuccessCheck', () => {
+        // testing netInfoSuccessCheck action with network connected
+        expect(
+            netInfo(undefined,{
+                type: netInfoSuccessCheck,
+                payload: {
+                    isConnected: false,
+                }
+            })
+        ).toEqual({isLoading:false,isConnected:false})
+
+        // testing netInfoSuccessCheck action with out network connected
+        expect(
+            netInfo(undefined,{
+                type: netInfoSuccessCheck,
+                payload: {
+                    isConnected: true,
+                }
+            })
+        ).toEqual({isLoading:false,isConnected:true})
+    })
+})
