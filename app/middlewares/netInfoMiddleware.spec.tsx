9bc6129f91550f2be67c6f94e13c0886323dc123
giuliobosco <giuliobva@gmail.com>
2020-05-04 17:53:07 +0200

    add netInfoMiddleware test

---
+++ app/middlewares/netInfoMiddleware.spec.tsx
@@ -0,0 +1,30 @@
+import netInfoMiddleware from './netInfoMiddleware';
+import { netInfoStartCheck, netInfoSuccessCheck } from '../components/main.component/netInfoSlice';
+
+const create = () => {
+    const store = {
+        dispatch: jest.fn(() => ({})),
+    };
+    const next = jest.fn();
+
+    const invoke = (action: any) => netInfoMiddleware(store)(next)(action)
+
+    return { store, next, invoke };
+}
+
+describe('Middleware::netInfo', () => {
+
+    it('calls test with causal action', () => {
+        const { next, invoke } = create()
+        const action = { type: 'Test' }
+        invoke(action)
+        expect(next).toHaveBeenCalledWith(action)
+    })
+
+    it('calls test with netInfoStartCheck action type', () => {
+        const { next, invoke } = create()
+        const action = { type: netInfoStartCheck.toString() }
+        invoke(action)
+        expect(next).toHaveBeenCalledWith(action)
+    })
+})
