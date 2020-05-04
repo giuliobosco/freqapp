399f7ee08e8ee135839e272a2acde18186f239fe
giuliobosco <giuliobva@gmail.com>
2020-05-04 09:43:27 +0200

    add freqline.component/freqlineStatusSlice

---
+++ app/components/freqline.component/freqlineStatusSlice.tsx
@@ -0,0 +1,55 @@
+import { createSlice } from '@reduxjs/toolkit';
+import FreqlineStatus from '../../models/FreqlineStatus';
+
+export const FreqlineStatus = {
+    freqlineOk: false,
+    serialOk: false,
+    dbOk: false,
+    isLoading: false,
+    error: '',
+}
+
+const freqlineStatusSlice = createSlice({
+    name: 'freqlineStatus',
+    initialState: FreqlineStatus,
+    reducers: {
+        freqlineStatusStartCheck: state => {
+            state.isLoading = true;
+            return state;
+        },
+        freqlineStatusSuccessCheck: (state, action) => {
+            state.isLoading = false;
+            if (action == null) {
+                state.error = 'no data';
+                return state;
+            }
+
+            const freqlineStatus: FreqlineStatus = action.payload;
+
+            if (freqlineStatus.freqline != null) {
+                state.freqlineOk = freqlineStatus.freqline;
+            }
+            if (freqlineStatus.serial != null) {
+                state.serialOk = freqlineStatus.serial;
+            }
+            if (freqlineStatus.db != null) {
+                state.dbOk = freqlineStatus.db;
+            }
+
+            return state;
+        },
+        freqlineStatusErrorCheck: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+            return state;
+        }
+    }
+});
+
+export const { 
+    freqlineStatusStartCheck, 
+    freqlineStatusSuccessCheck, 
+    freqlineStatusErrorCheck, 
+} = freqlineStatusSlice.actions;
+
+export default freqlineStatusSlice.reducer;
