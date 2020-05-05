72dae97988045ae906419ab6efa7278c45332c42
giuliobosco <giuliobva@gmail.com>
2020-05-05 11:23:42 +0200

    add loginMiddleware

---
+++ app/middlewares/loginMiddleware.spec.tsx
@@ -0,0 +1,65 @@
+import fetchMock from 'fetch-mock';
+
+import loginMiddleware from './loginMiddleware';
+import {
+    loginButtonClicked,
+    loginSuccessCheck,
+    loginErrorCheck,
+} from '../components/login.component/loginSlice'
+import Config from '../config';
+
+const create = () => {
+    const store = {
+        dispatch: jest.fn(() => { }),
+        getState: () => { return { username: 'hello', password: 'world' } }
+    }
+    const next = jest.fn();
+
+    const invoke = (action: any) => loginMiddleware(store)(next)(action)
+
+    return { store, next, invoke };
+}
+
+describe('Middleware::loginMiddleware', () => {
+    it('should call with casual action', () => {
+        const { next, invoke } = create();
+        const action = { type: 'Test' };
+        invoke(action);
+        expect(next).toHaveBeenCalledWith(action);
+    })
+
+    it('should be called next with loginButtonClicked', () => {
+        fetchMock.mock({
+            name: 'route',
+            matcher: Config.getApiBase() + 'action/login?username=hello&password=world',
+            method: 'POST',
+            response: {
+                status: 200,
+                body: []
+            }
+        })
+
+        const { next, invoke } = create();
+        const action = { type: loginButtonClicked.toString() };
+        invoke(action)
+        expect(next).toHaveBeenCalledWith(action);
+    })
+
+    it('should be called with loginSuccessCheck', () => {
+        const { next, invoke } = create();
+        const action = { type: loginSuccessCheck.toString() };
+        invoke(action);
+        expect(next).toHaveBeenCalledWith(action);
+    })
+
+    it('should be called with loginErrorCheck', () => {
+        const { next, invoke } = create();
+        const error = 'Error' + Math.random();
+        const action = {
+            type: loginErrorCheck.toString(),
+            payload: error,
+        }
+        invoke(action)
+        expect(next).toHaveBeenCalledWith(action);
+    })
+})
---
+++ app/middlewares/loginMiddleware.tsx
@@ -0,0 +1,24 @@
+import {
+    loginButtonClicked,
+    loginSuccessCheck,
+    loginErrorCheck,
+} from '../components/login.component/loginSlice';
+import { loginService } from '../services/AuthenticationService';
+
+const loginMiddleware = ({ dispatch, getState }: any) => (next: any) => (action: any) => {
+    if (action.type == loginButtonClicked.toString()) {
+        const { username, password } = getState();
+        loginService(username, password).then(() => {
+            dispatch(loginSuccessCheck());
+        }).catch(error => {
+            if (typeof error != 'string') {
+                error = "Network error"
+            }
+            dispatch(loginErrorCheck(error));
+        })
+    }
+
+    return next(action)
+}
+
+export default loginMiddleware;
