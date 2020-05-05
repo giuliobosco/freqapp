9a0e9ee931dca4befe5b58378bb9a25fee8ac0a7
giuliobosco <giuliobva@gmail.com>
2020-05-05 23:04:27 +0200

    add config/strings

---
+++ app/config/string.tsx
@@ -0,0 +1,9 @@
+const Strings = {
+    FREQLINE: 'Freqline',
+    FREQLINE_DESCRIPTION: 'Gestore di generatore di frequenze',
+    USERNAME_PLACEHOLDER: 'username',
+    PASSWORD_PLACEHOLDER: 'password',
+    LOGIN: 'login',
+}
+
+export { Strings };
