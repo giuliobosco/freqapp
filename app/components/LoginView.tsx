df97b260824e15730b92251d02e190535b691650
giuliobosco <giuliobva@gmail.com>
2020-05-05 23:05:38 +0200

    fix LoginView - textInput

--- app/components/LoginView.tsx
+++ app/components/LoginView.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import { View, StyleSheet, Text, TextInput } from "react-native";
+import React, { useRef } from 'react';
+import { View, StyleSheet, Text, KeyboardAvoidingView } from "react-native";
 import PropTypes from 'prop-types'
 
 import LoginButton from './LoginButton';
+import { Colors } from '../config/config';
+import { Strings } from '../config/string';
+import FormTextInput from './FormTextInput';
 
-const LoginView = ({usernameChanged, passwordChanged, buttonClicked, error}:any) => {
+const LoginView = ({ usernameChanged, passwordChanged, buttonClicked, error }: any) => {
     const year = new Date().getFullYear().toString();
+    const passwordInputRef = useRef();
+
+    const usernameSubmitPress = () => {
+        if (passwordInputRef.current) {
+            passwordInputRef.current.focus();
+        }
+    }
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Freqline</Text>
-            <Text style={styles.subtitle}>Gestore di generatore di frequenze</Text>
-        
-            <Text style={styles.inputName}>Username</Text>
-            <TextInput onChangeText={username => usernameChanged(username)} style={styles.input}/>
-            <Text style={styles.inputName}>Password</Text>
-            <TextInput onChangeText={password => passwordChanged(password)} style={styles.input}/>
-            <Text style={styles.error}>{error}</Text>
-            <LoginButton onPress={buttonClicked} text={'login'}/>
-            <Text style={styles.copy}>&copy;{year} Giulio Bosco</Text>
-        </View>
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>{Strings.FREQLINE}</Text>
+                <Text>{Strings.FREQLINE_DESCRIPTION}</Text>
+            </View>
+            <View style={styles.formContainer}>
+                <FormTextInput
+                    onChangeText={(username: string) => usernameChanged(username)}
+                    placeholder={Strings.USERNAME_PLACEHOLDER}
+                    keyboardType="url"
+                    autoCorrect={false}
+                    returnKeyLabel='next'
+                    autoCapitalize='none'
+                    onSubmitEditing={usernameSubmitPress} />
+                <FormTextInput
+                    ref={passwordInputRef}
+                    onChangeText={(password: string) => passwordChanged(password)}
+                    placeholder={Strings.PASSWORD_PLACEHOLDER}
+                    secureTextEntry={true}
+                    returnKeyType='done'
+                    onSubmitEditing={buttonClicked} />
+                {error != null ? <Text style={styles.error}>{error}</Text> : null}
+                <LoginButton onPress={buttonClicked} text={Strings.LOGIN} />
+                <Text>&copy;{year} Giulio Bosco</Text>
+            </View>
+        </KeyboardAvoidingView>
     );
 }
 
@@ -34,30 +59,31 @@ export default LoginView;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column',
-        alignItems: 'stretch',
-        justifyContent: 'center',
-        paddingHorizontal: 20
+        backgroundColor: Colors.WHITE,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    titleContainer: {
+        flex: 1,
+        width: "80%",
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        paddingTop: "20%",
     },
     title: {
-        fontSize: 40
+        fontSize: 40,
     },
-    subtitle: {
-        paddingTop: 15,
-        fontSize: 20
+    formContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        width: '80%',
     },
     inputName: {
         paddingTop: 30,
     },
-    input: { 
-        height: 40, 
-        borderColor: 'gray', 
-        borderBottomWidth: 1 
-    },
     error: {
-
-    },
-    copy: {
-
+        color: Colors.RED,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        marginBottom: 20,
     },
 });
--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Alert } from 'react-native';
 
-import FreqlineComponent from './FreqlineAppScreen';
-
-import LoadingView from '../views/LoadingView';
-import NoInternetConnectionView from '../views/NoInternetConnectionView';
-import loginService from '../services/LoginService';
 import LoginView from '../components/LoginView';
 
 const LoginComponent = () => {
 
     const viewProps = {
-        usernameChanged: (username:string) => {
+        usernameChanged: (username: string) => {
             console.log(username)
         },
-        passwordChanged: (password:string) => {
-            Alert.alert(password);
+        passwordChanged: (password: string) => {
+            console.log(password)
         },
         buttonClicked: () => {
             console.log('click')
