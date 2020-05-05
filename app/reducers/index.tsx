d03384f5051c2d3682a10c4d98bb188f57889d45
giuliobosco <giuliobva@gmail.com>
2020-05-05 11:24:50 +0200

    add app reducers - freqlineStatusReducer, loginReducer

--- app/reducers/index.tsx
+++ app/reducers/index.tsx
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import freqlineStatusReducer from '../components/freqline.component/freqlineStatusSlice';
 import netInfoReducer from '../components/main.component/netInfoSlice';
+import loginReducer from '../components/login.component/loginSlice';
 
 export default combineReducers({
     freqlineStatus: freqlineStatusReducer,
     netInfo: netInfoReducer,
+    login: loginReducer
 })
