82d585c2009f5e4282ec174f1e17f36da1e953d0
giuliobosco <giuliobva@gmail.com>
2020-05-06 16:05:34 +0200

    fix MainNavigationManager - permissions check

--- app/navigation/MainNavigationManager.tsx
+++ app/navigation/MainNavigationManager.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import _ from 'lodash';
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
 
 import FreqlineScreen from '../screens/FreqlineScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import UnauthorizedView from '../components/UnauthorizedView';
 import UsersNavigationManager from './UsersNavigationManager';
+
+import { permissionsStartCheck } from '../reducers/loginSlice';
+
 import { Strings } from '../config/string';
+import { Permissions } from '../config/config';
+import useFetching from '../utils/useFetching';
+import LoadingView from '../components/LoadingView';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,7 +26,22 @@ const MainTabs = {
     SETTINGS: 'Settings',
 }
 
-const MainNavigationManager = () => {
+const MainNavigationManager = ({ login, permissionsStartCheck }: any) => {
+
+    useFetching(permissionsStartCheck)
+
+    const { isLoadingPermission, permissions } = login;
+
+    console.log(permissions);
+
+    if (isLoadingPermission) {
+        return (<LoadingView />);
+    }
+
+    if (permissions.length === 0) {
+        return (<UnauthorizedView />);
+    }
+
     return (
         <NavigationContainer>
             <Tab.Navigator
@@ -40,15 +64,32 @@ const MainNavigationManager = () => {
                         // You can return any component that you like here!
                         return <Ionicons name={iconName} size={size} color={color} />;
                     },
+
                 })}>
-                <Tab.Screen name={MainTabs.FREQLINE} component={FreqlineScreen}  options={{ title: Strings.FREQLINE }}/>
-                <Tab.Screen name={MainTabs.USERS} component={UsersNavigationManager} options={{ title: Strings.USERS }}/>
-                <Tab.Screen name={MainTabs.SETTINGS} component={SettingsScreen} options={{ title: Strings.SETTINGS }}/>
+                {_.indexOf(permissions, Permissions.USER) > -1 ? <Tab.Screen name={MainTabs.FREQLINE} component={FreqlineScreen} options={{ title: Strings.FREQLINE }} /> : null}
+                {_.indexOf(permissions, Permissions.ADMIN) > -1 ? <Tab.Screen name={MainTabs.USERS} component={UsersNavigationManager} options={{ title: Strings.USERS }} /> : null}
+                {_.indexOf(permissions, Permissions.USER) > -1 ? <Tab.Screen name={MainTabs.SETTINGS} component={SettingsScreen} options={{ title: Strings.SETTINGS }} /> : null}
             </Tab.Navigator>
         </NavigationContainer>
     );
 }
 
+MainNavigationManager.prototype = {
+    login: PropTypes.object.isRequired,
+    permissionsStartCheck: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+    login: state.login,
+})
+
+const mapDispatchToProps = {
+    permissionsStartCheck,
+}
+
 export { MainTabs };
 
-export default MainNavigationManager;
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(MainNavigationManager);
