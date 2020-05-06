2e2c606d6b0fa56e64a071236dd5106c5c019c86
giuliobosco <giuliobva@gmail.com>
2020-05-06 18:10:54 +0200

    add statusService

--- app/services/FreqlineService.spec.tsx
+++ app/services/FreqlineService.spec.tsx
@@ -1,7 +1,7 @@
 import fetchMock from 'fetch-mock';
 
 import Config from '../config/config'
-import freqlineService from './FreqlineService';
+import freqlineService, { statusService } from './FreqlineService';
 
 describe('Service::FreqlineService', () => {
 
@@ -96,5 +96,88 @@ describe('Service::FreqlineService', () => {
         expect(catchFunction).toHaveBeenCalled();
 
         fetchMock.reset();
+    })
+})
+
+describe('Service::statusService', () => {
+
+    const name = 'action/generatorStatus';
+    const url = Config.getApiBase(name);
+
+    // testing getApiBase Get freqlineStatus
+    it('statusService.get(): should call api and return JSON', async () => {
+        const body = { ok: true, }
+        fetchMock.mock({
+            name: name+'/get _ok',
+            matcher: url,
+            method: 'GET',
+            response: {
+                status: 200,
+                body
+            }
+        })
+
+        await statusService.get().then(data => {
+            expect(data).toEqual(body)
+            fetchMock.reset();
+        })
+    })
+
+    it('statusService.get(): should\'t call thenFunction and should call catchFunction', async () => {
+        fetchMock.mock({
+            name: name+'/get _ok_func',
+            matcher: url,
+            method: 'GET',
+            response: {
+                status: 200,
+                body: {}
+            }
+        })
+
+        const thenFunction = jest.fn();
+        const catchFunction = jest.fn();
+
+        await statusService.get().then(thenFunction).catch(catchFunction)
+
+        expect(thenFunction).toHaveBeenCalled();
+        expect(catchFunction).not.toHaveBeenCalled();
+
+        fetchMock.reset();
+    })
+    
+    it('statusService.set(): should call api with 0 as status', async () => {
+        const body = { ok: true, }
+        fetchMock.mock({
+            name: name+'/set _ok_0',
+            matcher: url+'?status=0',
+            method: 'POST',
+            response: {
+                status: 200,
+                body
+            }
+        })
+
+        await statusService.set(false).then(data => {
+            expect(data).toEqual(body)
+            fetchMock.reset();
+        })
+    })
+
+    it('statusService.set(): should call api with 1 as status', async () => {
+        const body = { ok: true, }
+        fetchMock.mock({
+            name: name+'/set _ok_1',
+            matcher: url+'?status=1',
+            method: 'POST',
+            response: {
+                status: 200,
+                body
+            }
+        })
+
+        await statusService.set(true).then(data => {
+            expect(data).toEqual(body)
+            fetchMock.reset();
+        })
     })
 })
--- app/services/FreqlineService.tsx
+++ app/services/FreqlineService.tsx
@@ -14,3 +14,25 @@ export default function freqlineService() {
         return response.json();
     }).then(json => json);
 }
+
+const GET_HEADER = Config.getHeaders('GET');
+const POST_HEADER = Config.getHeaders('POST');
+
+const statusUrl = 'action/generatorStatus';
+
+const statusService = {
+    get: () => {
+        return fetch(Config.getApiBase(statusUrl), GET_HEADER).then(response => {
+            return response.json();
+        }).then(json => json); 
+    },
+    set: (status:boolean) => {
+        const url = Config.getApiBase(statusUrl) + '?status=' + (status?'1':'0');
+
+        return fetch(url, POST_HEADER)
+            .then(response => response.json())
+            .then(json => json);
+    }
+}
+
+export { statusService };
