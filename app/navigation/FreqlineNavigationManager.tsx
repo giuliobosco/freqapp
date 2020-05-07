930bef9e82216ded9e0b3df7c7da0f8189482dcc
giuliobosco <giuliobva@gmail.com>
2020-05-07 18:30:21 +0200

    add freqlineNavigationMangaer - saveButton

--- app/navigation/FreqlineNavigationManager.tsx
+++ app/navigation/FreqlineNavigationManager.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Button } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import PropTypes from 'prop-types';
 
 import FreqlineScreen from '../screens/FreqlineScreen';
 import { Strings } from '../config/string';
+import { freqlineStartPush }  from '../reducers/freqlineSlice';
+import { connect } from 'react-redux';
 
 const Stack = createStackNavigator();
 
@@ -10,14 +14,33 @@ const UsersTabs = {
     FREQLINE: 'freqline',
 }
 
-const FreqlineNavigationManager = () => {
+const FreqlineNavigationManager = ({freqlineStartPush}) => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name={UsersTabs.FREQLINE} component={FreqlineScreen} options={{ title: Strings.FREQLINE }}/>
+            <Stack.Screen name={UsersTabs.FREQLINE} 
+                component={FreqlineScreen} 
+                options={{ 
+                    title: Strings.FREQLINE,
+                    headerRight: () => (
+                        <Button onPress={() => freqlineStartPush()}
+                            title={Strings.SAVE}/>
+                    )
+                }}/>
         </Stack.Navigator>
     );
 }
 
+FreqlineNavigationManager.prototype = {
+    freqlineStartPush: PropTypes.func.isRequired,
+}
+
+const mapDispatchToProps = {
+    freqlineStartPush,
+}
+
 export { UsersTabs };
 
-export default FreqlineNavigationManager;
+export default connect(
+    null,
+    mapDispatchToProps,
+)(FreqlineNavigationManager);
